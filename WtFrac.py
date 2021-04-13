43b9f8ae585c44659a8d4afa14141f1e7165b947
Lam3name <36226992+Lam3name@users.noreply.github.com>
2021-04-13 15:01:45 +0200

    Added all files
    Started on task 1 coding

--- WtFrac.py
+++ WtFrac.py
@@ -1,13 +1,7 @@
 import scipy.optimize
 import numpy as np
 import matplotlib.pyplot as plt
-import WtFrac
 import constants as con
 
-def main():
-    print(WtFrac.WfFracCO2(con.alpha3))
-    
-    return 0
-
-
-main()
+def WfFracCO2(alpha):
+    return ((con.Mw[0])*alpha)/((1+(0.7/0.3))*con.MwMEA)
