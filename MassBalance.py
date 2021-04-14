79151facca7618540d37df5059b3dd79b95030ea
Lam3name <36226992+Lam3name@users.noreply.github.com>
2021-04-14 14:06:11 +0200

    Prøver å sette opp massebalanse

--- MassBalance.py
+++ MassBalance.py
@@ -2,10 +2,46 @@ import scipy.optimize
 import numpy as np
 import matplotlib.pyplot as plt
 import constants as con
+import WtFrac
 import compression
 
 def WtFracToAbsMols(tot, percent, molarMass):
     return((tot*percent)/molarMass*1000)     # mol/s
+
+
+def massStream(educatedGuess):
+    m1 = con.m1
+    mc1 = m1 * con.wc1
+    mh1 = m1 * con.wh1
+    mn1 = m1 * con.wn1
+    mo1 = m1 * con.wn1
+
+    #EducatedGuesses
+    m4  = educatedGuess[0]
+
+    mc2 = mc1 - mc1*con.wcapture
+    mc3 = WtFrac.WtFracCO2(con.alpha3)
+
+    #volum 1
+    mc4 = mc1 + mc3 - mc2
+    mh4 = mh1 + mh3 - mh2
+    mn4 = mn1 + mn3 - mn2
+    mo4 = mo1 + mo3 - mo2
+    mMEA4 = mMEA3
+    m4 = mc4 + mh4 + mn4 + mo4
+    #volum 2
+    mc5 = mc4
+    mMEA5 = mMEA4
+    mc3 = mc6
+    mMEA3 = mMEA6
+    #volum 3
+    mc9 = mc5 - mc6
+    mMEA6 = mMEA5
+
+    
+
+
+
 
 # Stream 1
 molsStream1 = [None]*5
--- WtFrac.py
+++ WtFrac.py
@@ -3,6 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import constants as con
 
-def WfFracCO2(alpha):
+def WtFracCO2(alpha):
     return ((con.Mw[0])*alpha)/((1+(0.7/0.3))*con.MwMEA)
 
