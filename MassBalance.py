cb885cc52965458f5ce620613f7d5aa02b0d2f31
Lam3name <36226992+Lam3name@users.noreply.github.com>
2021-04-14 13:06:58 +0200

    Update MassBalance.py

--- MassBalance.py
+++ MassBalance.py
@@ -8,11 +8,22 @@ def WtFracToAbsMols(tot, percent, molarMass):
     return((tot*percent)/molarMass*1000)     # mol/s
 
 # Stream 1
-molsStream1 = [None]*4 
+molsStream1 = [None]*5
 
-molsStream1[0] = WtFracToAbsMols(con.m1, con.wc1, con.Mw[0]) # mol/s
-molsStream1[1] = WtFracToAbsMols(con.m1, con.wh1, con.Mw[1]) # mol/s
-molsStream1[2] = WtFracToAbsMols(con.m1, con.wn1, con.Mw[2]) # mol/s
-molsStream1[3] = WtFracToAbsMols(con.m1, con.wo1, con.Mw[3]) # mol/s
+molsStream1[0] = WtFracToAbsMols(con.m1, con.wc1, con.Mw[0]) # CO2 mol/s
+molsStream1[1] = WtFracToAbsMols(con.m1, con.wh1, con.Mw[1]) # H2O mol/s
+molsStream1[2] = WtFracToAbsMols(con.m1, con.wn1, con.Mw[2]) # N2 mol/s
+molsStream1[3] = WtFracToAbsMols(con.m1, con.wo1, con.Mw[3]) # O2 mol/s
+molsStream1[4] = 0                                           # MEA mol/s
 
 print(molsStream1)
+
+
+# Stream 2
+molsStream2 = [None]*5
+
+molsStream1[0] = molsStream1[0] * con.alpha3 # CO2 mol/s
+molsStream1[1] = 0 # H2O mol/s
+molsStream1[2] = 0 # N2 mol/s
+molsStream1[3] = 0 # O2 mol/s
+molsStream1[4] = 0                                           # MEA mol/s
