a26cad89b8fc717706ad783ce531fe0853c62bf0
Lam3name <36226992+Lam3name@users.noreply.github.com>
2021-04-15 10:48:48 +0200

    AbsEnthalpy

--- enthalpy.py
+++ enthalpy.py
@@ -7,11 +7,14 @@ import constants as con
 def enthalpyWater(T2):
     T1 = 298
     integrand = lambda T: (5.05536*T - (T**2)*5.6552*10**(-3) - (T**3)*1.9*10**(-5))
-    
     integralCp = scipy.integrate.quad(integrand, T1, T2) #returns analytic result [0] and estimated error[1]
-    
     enthalpi = -13423.3 + integralCp[0]
     
     return enthalpi
 
 print(enthalpyWater(398))
+
+def AbsEnthalpyWater(T2, m):
+    return enthalpyWater(T2)*m
+
+print(AbsEnthalpyWater(398,45))
