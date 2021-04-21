c019398c6e37c10250e962da58551960903ab9de
BritaEK <82509436+BritaEK@users.noreply.github.com>
2021-04-21 11:00:31 +0200

    Update enthalpy.py

--- enthalpy.py
+++ enthalpy.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import scipy.integrate
 import numpy as np
 import matplotlib.pyplot as plt
@@ -45,3 +46,23 @@ def enthalpy_MEA_H20_CO2(T2):
 
 
 print(enthalpy_MEA_H20_CO2(393))
+=======
+
+
+import scipy.optimize
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def entalpivann(T2):
+    
+    T1 = 298
+    Cp = 5.05536*T - (T^2)*5.6552*10^(-3) - (T^3)*1.9*10^(-5)
+    integral_cp = scipy.integrate.quad(Cp, T1, T2)
+    
+    entalpi = -13423.3 + integral_cp
+    print(entalpi) 
+    return
+
+entalpivann(398)
+>>>>>>> Stashed changes
