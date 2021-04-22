526d41a359165fdef45886a34009028a4c6b5de4
Lam3name <36226992+Lam3name@users.noreply.github.com>
2021-04-22 20:16:48 +0200

    Update compression.py

--- compression.py
+++ compression.py
@@ -33,14 +33,27 @@ def realCompWorkUnknownTemp(T1, p1, p2, Cp):
 def heatExchanger(T1, T2, m, Cp):
     return m*Cp*(T2-T1)
 
+def axelWorkReal(T1, p1, p2, Cp):
+    return((Cp*0.001*T1)/con.eta)*(((p2/p1)**(con.gasConst/Cp))-1) #[KW/kg s^-1]
+
 def threeStageComp(Tinn, Tout, pinn, pout, m, Cp):
     Tb = tempAfterComp(Tinn, pinn, pb, Cp)
+    Wsb = axelWorkReal(Tinn, pinn, pb, Cp)
     Td = tempAfterComp(Tc, pc, pd, Cp)
+    Wsd = axelWorkReal(Tc, pc, pd, Cp)
     Tf = tempAfterComp(Te, pe, pf, Cp)
+    Wsf = axelWorkReal(Te, pe, pf, Cp)
+    work = [Wsb, Wsd, Wsf]
     temps = [Tb, Td, Tf]
-    return temps
+    return [temps, work]
 
-temps = threeStageComp(Ta, Tg, pa, pg, MassBalance.ans_m9, con.cpg[0])
+ans = threeStageComp(Ta, Tg, pa, pg, MassBalance.ans_m9, con.cpg[0])
+temps = ans[0]
+work = ans[1]
 print("Tb =",temps[0])
 print("Td =",temps[1])
 print("Tf =",temps[2])
+print("Wsb =",work[0])
+print("Wsd =",work[1])
+print("Wsf =",work[2])
+print("Tot Work =",(work[0]+work[1]+work[2]),"KW/kgs^-1")
